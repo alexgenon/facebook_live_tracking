04260b23c9ea1b40315047bbbf6c1ead053c42e0
Alexandre Genon <alexandre.genon@gmail.com>
2021-12-28 11:09:17 +0100

    Going from WireMock to MuServer

--- src/test/kotlin/be/aufildemescoutures/FacebookMockExtension.kt
+++ src/test/kotlin/be/aufildemescoutures/FacebookMockExtension.kt
@@ -1,13 +1,15 @@
 package be.aufildemescoutures
 
-import com.github.tomakehurst.wiremock.WireMockServer
+import io.muserver.*
+import io.muserver.MuServerBuilder.httpServer
+import io.muserver.handlers.ResourceHandlerBuilder
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager
-import com.github.tomakehurst.wiremock.client.WireMock.*
-import javax.ws.rs.core.MediaType
+import org.jboss.logging.Logger
 
 
 class FacebookMockExtension : QuarkusTestResourceLifecycleManager {
-    private lateinit var wireMockServer: WireMockServer
+    private lateinit var mockServer:MuServer
+    private val LOG= Logger.getLogger(FacebookMockExtension::class.java)
 
     private val comment_stream = """
     data:{"from":{"name":"Ariane Collard","id":"1088099037893657"},"created_time":"2021-12-19T05:38:29+0000","message":"je prends le 5","id":"461572928685550_461573375352172"}
@@ -17,25 +19,31 @@ class FacebookMockExtension : QuarkusTestResourceLifecycleManager {
 """.trimIndent()
 
     override fun start(): MutableMap<String, String> {
-        wireMockServer = WireMockServer()
-        wireMockServer.start()
-        val url =
-            "/1234/live_comments"
-        stubFor(
-            get(urlPathEqualTo(url))
-                .withQueryParam("access_token", equalTo("5678"))
-                .withQueryParam("fields", equalTo("from{name,id},created_time,message"))
-                .withQueryParam("comment_rate", equalTo("ten_per_second"))
-                .willReturn(okForContentType(MediaType.SERVER_SENT_EVENTS, this.comment_stream))
-        )
+        val url = "/1234/live_comments"
+        mockServer = httpServer()
+            .addHandler(
+                Method.GET,
+                url
+            ) { request: MuRequest?, response: MuResponse?, pathParams: Map<String?, String?>? ->
+                val publisher = SsePublisher.start(request, response)
+                Thread { count(publisher) }.start()
+            }
+            .start()
 
         return mutableMapOf(Pair
             ("quarkus.rest-client.\"be.aufildemescoutures.infrastructure.facebook.VideoStream\".url"
-            ,wireMockServer.baseUrl())
+            ,mockServer.httpUri().toString())
         )
     }
 
+    fun count(publisher: SsePublisher){
+        comment_stream.split("\n").forEach {
+            publisher.send(it)
+            Thread.sleep(500)
+        }
+        publisher.close()
+    }
     override fun stop() {
-        wireMockServer.stop()
+        mockServer.stop()
     }
 }
